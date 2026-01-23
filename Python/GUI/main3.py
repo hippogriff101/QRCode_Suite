890c701ea6277534c58b9cfdd83547d370b9ce2d
Freddie Yershon <freddieayershon@gmail.com>
2026-01-23 23:02:15 +0000

    Update a bunch of stuff!

--- Python/GUI/main3.py
+++ Python/GUI/main3.py
@@ -1,13 +1,15 @@
 from tkinter import *
 from tkinter import ttk
+from PIL import ImageTk, Image
 import qrcode
 import os
 
-number = 1
+number = 0
 path = os.getcwd()
 
 def main():
     global number
+    number += 1
     if coify.get().strip() == "":
         result_message.set("Please enter some text to generate a QR code.")
         return
@@ -15,7 +17,7 @@ def main():
     filename = f"{path}/qrcode_suite{number}.png" 
     img.save(filename)
     result_message.set(f"QR code generated and saved successfully as '{filename}'.")
-    number += 1
+
 def history():
     list_of_files = os.listdir(path)
     qr_files = [f for f in list_of_files if f.startswith("qrcode_suite") and f.endswith(".png")]
@@ -24,6 +26,20 @@ def history():
     else:
         history_message = "No QR code files found."
     result_message.set(history_message)
+def preview():
+    root = Toplevel()
+    if number == 0:
+        result_message.set("No QR code to preview.")
+        root.destroy()
+    else:
+        filename = f"qrcode_suite{number}.png"
+        ttk.Label(root, text=filename).grid(column=2, row=2, sticky=(W, E), padx=5, pady=5)
+        ttk.Button(root, text="Close", command=root.destroy).grid(column=2, row=4, sticky=(W, E), padx=5, pady=5)
+        img = ImageTk.PhotoImage(Image.open(filename))
+        panel = ttk.Label(root, image=img)
+        panel.image = img
+        panel.grid(column=2, row=3, sticky=(W, E), padx=5, pady=5)
+        root.mainloop()
 root = Tk()
 root.title("Basic QR Code Generator")
 root.columnconfigure(0, weight=1)
@@ -40,10 +56,10 @@ coify_entry.grid(column=2, row=1, sticky=(W, E), padx=5, pady=5)
 result_message = StringVar()
 result_message.set("Enter text and click QRify to generate a QR code.")
 ttk.Label(mainframe, textvariable=result_message).grid(column=2, row=2, sticky=(W, E), padx=5, pady=5)
-
 ttk.Button(mainframe, text="QRify", command=main).grid(column=3, row=3, sticky=W, padx=5, pady=5)
 ttk.Button(mainframe, text="History", command=history).grid(column=6, row=3, sticky=W, padx=5, pady=5)
-
+ttk.Button(mainframe, text="Preview", command=preview).grid(column=9, row=3, sticky=W, padx=5, pady=5)
+ttk.Button(mainframe, text="Close", command=root.destroy).grid(column=12, row=3, sticky=W, padx=5, pady=5)
 
 coify_entry.focus()
 root.mainloop()
--- README.md
+++ README.md
@@ -3,9 +3,7 @@
 A collection of differant ways to make QR Code's using python! This includes a version for terminal, desktop app, Discord and Slack! I will then make a website to host it all.
 
 > [!NOTE]
-> If you are reviewing for [Flavortown](flavortown.hackclub.com) Then please check the ship note, I'm working on the next ship while I'm waiting so there are more commits and stuff.
-## Docs
-_how to get it all working_
+> If you are reviewing for [Flavortown](flavortown.hackclub.com) Then please check the ship note, I'm working on differant versions of the project for differant ships.
 
 Pick the version you want to try out and follow the steps bellow!
 
@@ -23,7 +21,17 @@ pip install qrcode
 2. Run the `.exe` and follow instructions on screen
 
 ### GUI Mode
-(_still in development_)
+#### Local
+1. Make sure python is installed
+2. Install `qrcode`
+```
+pip install qrcode
+```
+3. Run `main3.py`
+4. Follow instructions on file to make QR Code
+#### Executable
+1. Download `QR_GUI.exe` from releases tab
+2. Run the `.exe` and follow instructions on screen
 
 
 ### Discord Bot
@@ -33,3 +41,7 @@ link to testing server
 ### Slack Bot
 (_in dev_)
 link to my public channel
+
+## AI Notice
+
+AI was used to debug code and to educate myself on features of the library the documentation was not clear on.
